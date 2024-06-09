53ee9d2196237b68c887d5697b22913f8c3fe5db
satyamounika11 <satya.mounika@gmail.com>
2024-06-09 23:10:39 +0530

    terraform files added

---
+++ terraform-ecs-fargate/main.tf
@@ -0,0 +1,104 @@
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = var.ecs_task_execution_role_name
+}
+
+data "aws_security_group" "existing_lb_security_group" {
+  id = var.existing_lb_security_group_id
+}
+
+data "aws_security_group" "existing_ecs_security_group" {
+  id = var.existing_ecs_security_group_id
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = var.subnet_id_for_nat_gateway
+}
+
+resource "aws_route" "private_route" {
+  route_table_id         = var.private_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+}
+
+resource "aws_ecs_cluster" "nodejs_cluster" {
+  name = "nodejs-cluster"
+}
+
+resource "aws_ecs_task_definition" "nodejs_task" {
+  family                   = "nodejs-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "nodejs-container"
+    image     = var.nodejs_image
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "nodejs_service" {
+  name            = "nodejs-service"
+  cluster         = aws_ecs_cluster.nodejs_cluster.id
+  task_definition = aws_ecs_task_definition.nodejs_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.nodejs_tg.arn
+    container_name   = "nodejs-container"
+    container_port   = 3000
+  }
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [data.aws_security_group.existing_ecs_security_group.id]
+  }
+}
+
+resource "aws_lb" "nodejs_lb" {
+  name               = "nodejs-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [data.aws_security_group.existing_lb_security_group.id]
+  subnets            = var.public_subnets
+}
+
+resource "aws_lb_target_group" "nodejs_tg" {
+  name       = "nodejs-tg"
+  port       = 3000
+  protocol   = "HTTP"
+  target_type = "ip"
+  vpc_id     = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "nodejs_listener" {
+  load_balancer_arn = aws_lb.nodejs_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nodejs_tg.arn
+  }
+}
+
